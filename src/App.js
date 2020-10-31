679ee4031321abed2811387a3cd5800917695723
Andrea Parra <aparra001@gmail.com>
2020-10-31 16:45:21 -0300

    entrega To do list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [state, setState] = useState([{ task: "Tarea inicio" }]);
+  const [task, setTask] = useState("");
+
+  // const enter = (e) => {
+  //   var x = e.which || e.keyCode;
+  //   console.log(x);
+  //   if (x === 13) { setState([...state, e.target.value]) }
+  // }
+  // console.log(state);
+
+  function deleteItem(index) {
+    const filterTask = state.filter((item) => {
+      return item !== state[index]
+    })
+    setState(filterTask);
+    console.log("filtertask", filterTask);
+  }
+
+  // funcion que guarda el dato en el arreglo
+
+  function handlerSubmit(e) {
+    e.preventDefault()
+    setState([
+      ...state,
+      { task: task }
+    ])
+  }
+  //console.log(task);
+  console.log(state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container center">
+      <h1>TO DO LIST</h1>
+
+      <form onSubmit={e => handlerSubmit(e)}>
+        <input
+          placeholder="Nueva tarea"
+          type="text"
+          onChange={e => setTask(e.target.value)} />
+      </form>
+
+
+      <ul  style={{listStyle:"none"}}>
+        {state.map((item, index) =>
+          <li>{item.task}
+            <a style={{marginLeft:"20px", fontWeight:"bold"}} onClick={() => deleteItem(index)}>  
+              X
+            </a>
+          </li>)
+        }
+      </ul>
+    </div >
   );
 }
+
+
 
 export default App;
